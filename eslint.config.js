b07f9bd220be776688cfd1dffe197a1c6b2fa2d7
烨笙 <279277332@qq.com>
2025-12-31 07:39:42 +0000

    update eslint.config.js.
    Signed-off-by: 烨笙 <279277332@qq.com>

--- eslint.config.js
+++ eslint.config.js
@@ -26,7 +26,8 @@ export default [
         ignores: [
             "node_modules/*",
             "tests/*",
-            "src/icons/*"
+            "src/icons/*",
+            "docs/*"
         ]
     }
 ];
